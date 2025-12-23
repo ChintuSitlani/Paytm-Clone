bb1a54663b9fd03a16d831c340b05ee36388e908
chintusitlani <chintusitlani09@gmail.com>
2025-12-23 19:37:22 +0530

    removed docs and renamed app to user-app

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
         <p className="fixed top-0 left-0 flex justify-center w-full px-4 pt-8 pb-6 border backdrop-blur-2xl border-neutral-800 from-inherit lg:static lg:w-auto lg:rounded-xl lg:p-4">
           examples/with-tailwind -&nbsp;
-          <code className="font-mono font-bold">docs</code>
+          <code className="font-mono font-bold">web</code>
         </p>
         <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 lg:static lg:h-auto lg:w-auto">
           <a
@@ -54,7 +54,6 @@ export default function Page() {
           </a>
         </div>
       </div>
-
       <div className="relative flex place-items-center ">
         <div className="font-sans w-auto pb-16 pt-[48px] md:pb-24 lg:pb-32 md:pt-16 lg:pt-20 flex justify-between gap-8 items-center flex-col relative z-0">
           <div className="z-50 flex items-center justify-center w-full">
