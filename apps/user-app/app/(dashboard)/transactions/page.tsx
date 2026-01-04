a44edd1dfc2ad40b6e95d30908af0e4d37e2c9ce
chintusitlani <chintusitlani09@gmail.com>
2026-01-04 22:39:29 +0530

    -fixed pages and their UI

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -1,6 +1,10 @@
 
-export default function() {
-    return <div>
-        Transactions
-    </div>
+export default function () {
+    return(
+        <div className="w-screen">
+            <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+                Transactions
+            </div>
+        </div>
+    )
 }
