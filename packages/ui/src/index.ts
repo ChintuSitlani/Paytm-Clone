1e5b0593badc3752f8cb45da036b8d602e9be85b
chintusitlani <chintusitlani09@gmail.com>
2026-01-09 21:41:29 +0530

    ------ package.json changes-------- -removed package-lock.json\ -added new devDependencies --,"typescript":"^5.3.3","@types/node": "^20.11.24", - replaced @repo/----- values by removing "*" with  "workspace:*" -added new exports from eslint-config/package.json and db/package.json -----Other changes------- ----pnpm----- -added pnpm-workspace.yaml file -generated pnpm-lock.yaml file ----tsconfig----- -removed files key db tsconfig.json -added more compilerOptions in ui ts config -----packages/ui changes---- -added new file index.ts for exporting all the tsx files

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,8 @@
+// packages/ui/src/index.ts
+export * from "./button";
+export * from "./card";
+export * from "./code";
+export * from "./Appbar";
+export * from "./Center";
+export * from "./Select";
+export * from "./TextInput";
